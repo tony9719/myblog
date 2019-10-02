f1d6a681777d06824e9a7a4cece6caec08e849f4
TONY DE VITA <tony.devita97@studenti.unipr.it>
2019-10-02 21:26:45 +0200

    last commit

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,10 @@
+module.exports = [{
+      plugin: require('C:/Users/tonyj/Desktop/PERSONALE/PROGETTI/BLOG/template/blog/myblog/node_modules/gatsby-remark-images/gatsby-browser.js'),
+      options: {"plugins":[],"maxWidth":1170,"quality":90},
+    },{
+      plugin: require('C:/Users/tonyj/Desktop/PERSONALE/PROGETTI/BLOG/template/blog/myblog/node_modules/gatsby-plugin-canonical-urls/gatsby-browser.js'),
+      options: {"plugins":[],"siteUrl":"https://tony9719.github.io/myblog/"},
+    },{
+      plugin: require('C:/Users/tonyj/Desktop/PERSONALE/PROGETTI/BLOG/template/blog/myblog/node_modules/gatsby-plugin-google-analytics/gatsby-browser.js'),
+      options: {"plugins":[],"trackingId":"UA-XXXX-Y","head":true,"anonymize":true,"respectDNT":true,"exclude":["/preview/**"],"sampleRate":100,"siteSpeedSampleRate":10},
+    }]
